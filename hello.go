5a6f9072886b930221da36bd070092e5b83a5971
sharifulb07 <sharifulb07@gmail.com>
2025-11-14 19:06:04 +0600

    go variable and constant

--- hello.go
+++ hello.go
@@ -12,9 +12,132 @@
 package main
 
 import "fmt"
-import "rsc.io/quote"
+
 
 func main() {
-    fmt.Println("Hello world")
-    fmt.Println(quote.Go())
+    
+    varSec()
+}
+
+
+func varSec(){
+
+
+// rune used for unicode in go lang 
+    var r rune = 'গ'
+    fmt.Println(r)
+
+
+
+
+    // pointer in  go lang
+
+    // var x=50
+    // var y=&x
+    // var z=&y
+    // fmt.Println(y)
+    // fmt.Println(x)
+    // fmt.Println(z)
+
+
+
+
+
+
+
+    // map in go lang
+
+    // names:=map[string]string{
+    //     "one":"sharif",
+    //     "two":"adi",
+    //     "three":"ashia",
+    // }
+
+    // for _, value:= range names{
+    //     fmt.Println(value)
+    // }
+    // fmt.Println(names)
+
+
+
+
+
+    // array in go 
+
+// numbers :=[]int {40,50,60,70}
+// fmt.Println(numbers)
+
+
+
+
+
+
+    // var arr []int=[]int{1,2,3,4,5,6,7}
+    // for index, value:=range arr{
+
+    //     fmt.Println(index, value)
+    // }
+
+
+
+
+
+    
+    // constant in go lang
+
+
+    // const (
+    //     A=iota
+    //     B
+    //     C
+    //     D
+    //     E
+    //     F
+    // )
+
+    // fmt.Println(A,B,C, D, E, F)
+
+
+
+
+
+
+
+    // const PI=3.14159
+    // const name="shariful"
+
+    // fmt.Println(PI, name)
+
+
+
+    // variable in go lang
+    
+    // var (
+    //     username string="cureone"
+    //     isLoggin bool=false
+    //     score int=32
+    // )
+
+    // fmt.Println(username, isLoggin, score)
+
+
+
+
+    // var friend string
+    // var price int
+    // var confident bool
+
+    // friend="Adiyat"
+    // price=07
+    // confident=true
+    // fmt.Println(friend, price, confident)
+
+// var x, y, z=10,20,30
+// fmt.Println(x,y,z)
+
+    // name :="Shariful Islam"
+    // age :=37
+    // city :="Khulna"
+
+    // fmt.Println(name,"who is", age, "years old lives in", city, "City")
 }
