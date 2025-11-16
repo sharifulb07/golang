73bc2cc662c55fef7e6bdd28c11cdfe6efc89c27
sharifulb07 <sharifulb07@gmail.com>
2025-11-16 11:02:19 +0600

    go functions in details examples

--- hello.go
+++ hello.go
@@ -1,3 +1,88 @@
+
+
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+
+test()
+  
+
+}
+
+
+func test(){
+    defer fmt.Println("Last")
+    fmt.Println("First")
+}
+
+
+// my:=Person{name:"Shariful islam", age:36}
+// my.greet()
+   
+
+// Methods (Functions with Receiver)
+// type Person struct{
+//     name string
+//     age int
+// }
+
+// func (p Person) greet(){
+//     fmt.Printf("I am %v and i am %v years old", p.name, p.age)
+// }
+
+// c:=counter()
+// fmt.Println(c())
+// fmt.Println(c())
+// fmt.Println(c())
+// fmt.Println(c())
+// fmt.Println(c())
+
+
+
+// a closure function 
+// func counter() func() int{
+//     count:=-1
+//     return func()int{
+//         count++
+//         return count
+//     }
+// }
+
+// describer:=multiplier(10)
+// fmt.Println(describer(50))
+// f:=func(a int, b int)int {
+//     return a+b
+// }
+
+// fmt.Println(f(20,50))
+
+
+
+//     var fullname, country string
+// var age int
+// var gpa float32
+
+// fmt.Print("Enter Your Info: ")
+// fmt.Scan(&fullname, &age, &country, &gpa)
+
+// fmt.Printf("%v is a student \n", fullname)
+// fmt.Printf("%v is %v years old. \n", fullname, age)
+// fmt.Printf("%v has got GPA %v \n", fullname, gpa)
+// fmt.Printf("%v is from %v\n", fullname, country)
+
+  // Function Returning Another Function
+//   func multiplier (x int) func(int) int{
+//     return func(n int)int {
+//         return x*n
+//     }
+
+// }
+
+
 // package main
 
 // import "fmt"
@@ -9,23 +94,155 @@
 // 	fmt.Println(quote.Go())
 // }
 
-package main
 
-import "fmt"
+// variadic func with unlimited arguments
+
+// func sum(nums ...int)int {
+//     total:=0
+
+//     for _, value:= range nums{
+//         total+=value
+//     }
+//     return total 
+// }
 
 
-func main() {
-    
-    varSec()
-}
+
+// function with named return values
+
+// func rectangle(w, h float64)(area, peremiter float64){
+//     area=w*h
+//     peremiter=2*(w+h)
+//     return
+// }
 
 
-func varSec(){
+// func divide(a, b float64)( float64, error){
+//     if b==0{
+//         return 0, fmt.Errorf("divided by zero")
+//     }
+//     return a/b, nil
+// }
+
+
+// func add(a int, b int)int{
+//     return a+b
+// }
+
+
+// func varSec(){
+
+
+//     fmt.Println("Be Cool Man: ")
+
+// name:="sharif"
+
+// result:=fmt.Sprintf("This is %v", name)
+// fmt.Println(result)
+
+
+// loop in map
+
+
+
+    // for i, c :=range "Golang"{
+    //     fmt.Println(i, string(c))
+    // }
+
+    // mymap:=map[string]int{"a":1, "b":3,"c":5}
+
+
+    // for i,v :=range mymap{
+    //     fmt.Println(i, v)
+    // }
+
+
+
+
+    // using loop in array
+
+    // arr:=[]int{1,11,111,1111,2,22,222,2222}
+
+    // for index, value:=range arr{
+    //     fmt.Println(index, value)
+    // }
+
+
+    // loop statement
+
+    // for i:=0;i<=5;i++{
+    //     fmt.Println(i)
+    // }
+
+
+    // while loop
+    // j:=6
+
+    // for j<=20{
+    //     fmt.Println("Value",j)
+    //     j++
+    // }
+
+
+    // switch statement
+
+    // score:=65
+
+    // switch{
+    // case score>=80:
+    //     fmt.Println("A Grade")
+    // case score>=70:
+    //     fmt.Println("B Grade")
+    // case score>=60:
+    //     fmt.Println("C Grade")
+    // }
+
+
+    // fruit:="apple"
+
+    // switch fruit{
+    // case "mango":
+    //     fmt.Println("Mango")
+    // case "apple":
+    //     fmt.Println("Apple")
+    // default:
+    //     fmt.Println("Unknown Fruit")
+    // }
+
+    // switch day:=2; day{
+    // case 1,2,3,4,5:
+    //     fmt.Println("Weekday")
+    // case 6,7:
+    //     fmt.Println("Weekend")
+    // default:
+    //     fmt.Println("Invalid Number ")
+    // }
+
+
+    // if conditional statement
+
+// age:=22
+
+// if age>=21{
+//     fmt.Println("Adult")
+// }else{
+//     fmt.Println("Minor")
+// }
+
+
+// if num:=11; num%2==0{
+//     fmt.Println("Even Number")
+// }else{
+//     fmt.Println("Odd Number")
+// }
+
+
+
 
 
 // rune used for unicode in go lang 
-    var r rune = 'গ'
-    fmt.Println(r)
+    // var r rune = 'গ'
+    // fmt.Println(r)
 
 
 
@@ -140,4 +357,4 @@ func varSec(){
     // city :="Khulna"
 
     // fmt.Println(name,"who is", age, "years old lives in", city, "City")
-}
+// }
