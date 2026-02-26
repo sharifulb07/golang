c7acd02c6b2886b4ce6af8bb1efe749e1e65359c
sharifulb07 <sharifulb07@gmail.com>
2026-02-26 20:17:15 +0600

    higher order function example

--- day12/practice.go
+++ day12/practice.go
@@ -4,8 +4,21 @@ import "fmt"
 func main() {
 
 	fmt.Println("Hello, World!")
+	fmt.Println("The result of addition is ", processOp(10, 20, add))
 }
 
-func  init()  {
-	fmt.Println("init function called")
+
+func processOp(a int, b int, op func (x int, y int)int) int {
+
+	fmt.Println("Process Operation works very well now ")
+	return op(a, b)
+	
+}
+
+
+
+func add(a int, b int) int {
+
+	return a + b
+
 }
