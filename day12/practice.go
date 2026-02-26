159e0253870376e83a871bce2d970031a7419134
sharifulb07 <sharifulb07@gmail.com>
2026-02-26 22:09:35 +0600

    http secure get function

--- day12/practice.go
+++ day12/practice.go
@@ -1,20 +1,39 @@
-package main
-
+package main 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
-
-func handler(w http.ResponseWriter, r *http.Request){
-
-	fmt.Fprintln(w, "Server is runing very well ")
-}
 
 
 
 func main(){
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/hello", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
 
+
+func handler(w http.ResponseWriter, r *http.Request){
+
+	if(r.Method!=http.MethodGet){
+		http.Error(w, "Method not allowerd", http.StatusMethodNotAllowed)
+		return 
+	}
+
+	name:=r.URL.Query().Get("name")
+
+
+	if len(name)==0{
+		http.Error(w,"Name is required", http.StatusBadRequest)
+		return 
+	}
+
+	if len(name)>50{
+		http.Error(w,"Name is too long", http.StatusBadRequest)
+		return 
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintf(w, "Hello, %s!", name)
 }
