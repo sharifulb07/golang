7837ca48b6a92f569880244349dbdc0ddc7e6cbd
sharifulb07 <sharifulb07@gmail.com>
2026-02-27 21:25:51 +0600

    rename file, delete file and check it exist file

--- day13/fileoperations.go
+++ day13/fileoperations.go
@@ -1,18 +1,59 @@
-package main 
+package main
+
 import (
-	
+	"fmt"
 	"os"
 )
 
 
 func main(){
-	file, _:=os.Create("test.txt")
-	defer file.Close()
+	// file, _:=os.Create("test.txt")
+	// defer file.Close()
 
-	file.WriteString("Hello Go. I am learning you! \n")
+	// file.WriteString("Hello Go. I am learning you! \n")
 
-	data:=[]byte("Byte content \n")
-	file.Write(data)
+	// data:=[]byte("Byte content \n")
+	// file.Write(data)
+
+	// fmt.Println("file creation")
+
+
+	// f,_:=os.OpenFile("test.go", os.O_RDWR, 0644)
+
+	// defer f.Close()
+
+
+	// appending file
+
+	// f,_ :=os.OpenFile("test.txt", os.O_APPEND|os.O_WRONLY, 0644)
+	// defer f.Close()
+	// f.WriteString("Appending new line \n")
+
+
+	// delete file
+
+// os.Remove("test.txt")
+
+// fmt.Println("Remove test file here ")
+
+	
+// os.Rename("test.txt", "new_text.txt")
+
+
+// info,err:=os.Stat("new_test.txt")
+// if os.IsNotExist(err){
+// 	fmt.Println("File does not exist")
+// }
+
+
+// fmt.Println(info.Name(), info.Size())
+
+
+
+
+
+
+
 	
 
 
