a2acd926b724b460712ff041f4f34dcfa920bf66
sharifulb07 <sharifulb07@gmail.com>
2026-03-03 23:49:08 +0600

    slice and binary search algorithm

---
+++ day14/practice.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	
+)
+
+
+func main(){
+
+
+	// slice or dynamic array
+
+	// sr:= []string{"a","b","c", "d", "e", "f"}
+
+	
+	//  fmt.Println(sr[2:])
+
+
+
+
+	
+
+	// fmt.Println("actual value", a)
+	// fmt.Println("reference value", b)
+
+
+	// fmt.Println(sr[0])
+	// fmt.Println(sr[1])
+	// fmt.Println(sr[2])
+	// fmt.Println(sr[3])
+	// fmt.Println(sr[4])
+
+
+
+// pointer
+// len
+// cap
+
+
+
+
+	
+
+
+
+}
